df69b77bd87d4c78ca488317a80938bfad761c3c
kanggiho <kgh5554@naver.com>
2024-11-25 14:05:17 +0900

    orderer연결

--- src/main/java/org/example/Project1/__Main/Main.java
+++ src/main/java/org/example/Project1/__Main/Main.java
@@ -5,6 +5,6 @@ import org.example.Project1._Frame.Login;
 public class Main {
     public static void main(String[] args) throws Exception {
         new Login();
-        System.out.println("완성");
+        //System.out.println("완성");
     }
 }
