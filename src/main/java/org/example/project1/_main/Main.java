9adabeee72f35fd32c25cd8ecc1663d73252af96
kanggiho <kgh5554@naver.com>
2024-11-29 14:34:22 +0900

    package update

--- src/main/java/org/example/project1/_main/Main.java
+++ src/main/java/org/example/project1/_main/Main.java
@@ -1,6 +1,7 @@
 package org.example.project1._main;
 
-import org.example.project1._common.View.Frame.LoginFrame;
+
+import org.example.project1.user.UI.LoginFrame;
 
 public class Main {
     public static void main(String[] args) throws Exception {
