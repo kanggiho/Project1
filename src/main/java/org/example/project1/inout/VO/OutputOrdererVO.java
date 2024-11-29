3d3de805105fae4e96ac4502476faac653d443af
양혜선 <hspob819b@gmail.com>
2024-11-29 18:50:07 +0900

    주석 및 @setter, getter, tostring 추가

--- src/main/java/org/example/project1/inout/VO/OutputOrdererVO.java
+++ src/main/java/org/example/project1/inout/VO/OutputOrdererVO.java
@@ -1,12 +1,19 @@
 package org.example.project1.inout.VO;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
 public class OutputOrdererVO {
-   private int user_id;
-   private String license;
-   private String name;
-   private String tel;
-   private int confirm_num;
-   private int confirm_id;
-   private String status;
-   private String release_date;
+   private int user_id; // 주문자명
+   private String license; // 사업자등록번호
+   private String name; // 주문자 이름
+   private String tel; // 주문자 전화번호
+   private int confirm_num; // 승인 번호
+   private int confirm_id; // 결재자
+   private String status; // 승인여부
+   private String release_date; // 출고량
 }
