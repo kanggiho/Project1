f6ce891b7d1b833e2e30037d2f3ec95bf29adc66
kanggiho <kgh5554@naver.com>
2024-11-29 14:20:23 +0900

    package update

--- src/main/java/org/example/project1/_main/Main.java
+++ src/main/java/org/example/project1/_main/Main.java
@@ -1,12 +1,35 @@
 package org.example.project1._main;
 
+
 import org.example.project1._common.View.Frame.LoginFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        new LoginFrame();
-        //new RegistrationFrame();
-        //new RecoveryAccountFrame();
 
+
+        Color color = new Color(0x5A2121);
+
+
+
+        try {
+            // Look and Feel 설정
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) { // Nimbus가 설치된 Look and Feel 중 선택
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 스윙 컴포넌트 생성
+        SwingUtilities.invokeLater(() -> {
+            new LoginFrame();
+        });
     }
 }
