711892c66235b7bf5b192aae4fc43cf13da3274d
Kjo <krt8599@naver.com>
2024-11-30 21:32:06 +0900

    InventorySearchServiceTest

--- src/main/java/org/example/project1/inventory/test/InventorySearchServiceTest.java
+++ src/main/java/org/example/project1/inventory/test/InventorySearchServiceTest.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class InventorySearchServiceTest {
     public static void main(String[] args) {
         ProductInfoDAO dao = new ProductInfoDAO();
