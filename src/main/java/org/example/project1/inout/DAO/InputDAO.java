eb579fb66b703a0b6abe67d1e5bf94187cb726c2
양혜선 <hspob819b@gmail.com>
2024-12-02 09:33:12 +0900

    입고 신청 패널 작업(입고 신청 조회) 및 VO, DAO 수정

--- src/main/java/org/example/project1/inout/DAO/InputDAO.java
+++ src/main/java/org/example/project1/inout/DAO/InputDAO.java
@@ -1,6 +1,8 @@
 package org.example.project1.inout.DAO;
 
 import org.example.project1._common._Ut.HikariCPDataSource;
+import org.example.project1.inout.VO.InputManuVO;
+import org.example.project1.inout.VO.InputProductVO;
 import org.example.project1.inout.VO.InputVO;
 
 import javax.sql.DataSource;
@@ -15,6 +17,8 @@ public class InputDAO {
     }
 
     ArrayList<InputVO> vo_list = new ArrayList<>();
+    ArrayList<InputProductVO> vo_listForProduct = new ArrayList<>();
+    ArrayList<InputManuVO> vo_listForManu = new ArrayList<>();
 
     public void insertToInput(InputVO vo) {
         try (Connection con = dataSource.getConnection();
@@ -57,10 +61,86 @@ public class InputDAO {
         }
     }
 
+    //자재코드 기준 검색 결과
+    public ArrayList<InputProductVO> listForProductCode(int product_code) {
+        try (Connection con = dataSource.getConnection();
+             PreparedStatement ps = con.prepareStatement
+                     ("select p.product_code, p.product_name, i.input_num, " +
+                     "i.manufacturer_code, i.asking_date, " +
+                     "i.warehoused_quantity, i.warehoused_date " +
+                     "from product p " +
+                     "left join input i " +
+                     "on i.product_code = p.product_code " +
+                     "where product_code = ?")) {
+            ps.setInt(1, product_code);
+            ResultSet table = ps.executeQuery();
+            if (!vo_listForProduct.isEmpty()) {
+                vo_listForProduct.clear();
+            }
+            while (true) {
+                if (table.next()) {
+                    InputProductVO vo = new InputProductVO();
+                    vo.setProduct_code(table.getInt("product_code"));
+                    vo.setProduct_name(table.getString("product_name"));
+                    vo.setInput_num(table.getInt("input_num"));
+                    vo.setManufacturer_code(table.getString("manufacturer_code"));
+                    vo.setAsking_date(table.getString("asking_date"));
+                    vo.setWarehoused_quantity(table.getInt("warehoused_quantity"));
+                    vo.setWarehoused_date(table.getString("warehoused_date"));
+                    vo_listForProduct.add(vo);
+                } else {
+                    break;
+                }
+            }
+            return vo_listForProduct;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
 
+    //제조업체 코드 기준 검색 결과
+    public ArrayList<InputManuVO> listForManu (String manufacturer_code) {
+        try (Connection con = dataSource.getConnection();
+             PreparedStatement ps = con.prepareStatement
+                     ("select m.manufacturer_code, m.manufacturer_name, m.sorting, i.input_num, i.product_code, " +
+                             "i.asking_date, i.warehoused_quantity, i.warehoused_date" +
+                             "from manufacturing m" +
+                             "left join input i" +
+                             "on i.manufacturer_code = m.manufacturer_code" +
+                             "where input_num is not null and manufacturer_code = ?")) {
+            ps.setString(1, manufacturer_code);
+            ResultSet table = ps.executeQuery();
+            if (!vo_listForManu.isEmpty()) {
+                vo_listForManu.clear();
+            }
+            while (true) {
+                if (table.next()) {
+                    InputManuVO vo = new InputManuVO();
+                    vo.setManufacturer_code(table.getString("manufacturer_code"));
+                    vo.setManufacturer_name(table.getString("manufacturer_name"));
+                    vo.setSorting(table.getString("sorting"));
+                    vo.setInput_num(table.getInt("input_num"));
+                    vo.setProduct_code(table.getInt("product_code"));
+                    vo.setAsking_date(table.getString("asking_date"));
+                    vo.setWarehoused_quantity(table.getInt("warehoused_quantity"));
+                    vo.setWarehoused_date(table.getString("warehoused_date"));
+                    vo_listForManu.add(vo);
+                } else {
+                    break;
+                }
+            }
+            return vo_listForManu;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    //입고신청일 기준 검색 결과
     public ArrayList<InputVO> list(String warehousedDate) {
         try (Connection con = dataSource.getConnection();
-             PreparedStatement ps = con.prepareStatement("select * from input where warehoused_date = ? and product_code = ?")) {
+             PreparedStatement ps = con.prepareStatement("select * from input where warehoused_date = ?")) {
             ps.setString(1, warehousedDate);
             ResultSet table = ps.executeQuery();
             if (!vo_list.isEmpty()) {
@@ -69,10 +149,10 @@ public class InputDAO {
             while (true) {
                 if (table.next()) {
                     InputVO vo = new InputVO();
+                    vo.setAskingDate(table.getString("asking_date"));
                     vo.setInputNum(table.getInt("input_num"));
                     vo.setManufacturerCode(table.getString("manufacturer_code"));
                     vo.setProductCode(table.getInt("product_code"));
-                    vo.setAskingDate(table.getString("asking_date"));
                     vo.setWarehousedQuantity(table.getInt("warehoused_quantity"));
                     vo.setWarehousedDate(table.getString("warehoused_date"));
                     vo_list.add(vo);
--- src/main/java/org/example/project1/inout/UI/IncomingManagementPanel.java
+++ src/main/java/org/example/project1/inout/UI/IncomingManagementPanel.java
@@ -1,50 +1,230 @@
 package org.example.project1.inout.UI;
 
+
+import org.example.project1.inout.DAO.InputDAO;
+import org.example.project1.inout.VO.InputManuVO;
+import org.example.project1.inout.VO.InputProductVO;
+import org.example.project1.inout.VO.InputVO;
+
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.util.ArrayList;
+
 
 public class IncomingManagementPanel extends JPanel {
+    private JTextField inputField;
+    private JComboBox<String> modeSelector;
+    private JTable resultTable;
+    private String selectMode;
 
     public IncomingManagementPanel() {
         setPanel();
         initUI(); // UI 초기화
     }
 
+    //Panel 설정
     private void setPanel() {
-
+        setSize(1100, 450);
+        setBackground(Color.decode("#97A6A0"));
+        setLayout(null);
+        setVisible(true);
     }
 
     // UI 초기화
     private void initUI() {
-        JLabel Label = new JLabel();
-        setLayout(new BorderLayout());
-        //add(Label, BorderLayout.CENTER);
+        //검색창 생성(자재코드, 제조업체 코드, 입고신청일 기준으로 검색)
+        inputField = new JTextField(20);
 
-        // JTextField- 검색창
-        // 자재코드 기준으로 검색
-        // 제조업체 코드 기준으로 검색
-        // 입고신청일 기준으로 검색
-        JTextField inputProductCode = new JTextField();
+        //데이터 테이블 생성
+        resultTable = new JTable();
+        JScrollPane scrollPane = new JScrollPane(resultTable);
 
 
+        //모드 선택 기능 구현
+        String[] modes = {"전체보기", "자재코드", "제조업체", "입고신청일"};
+        modeSelector = new JComboBox<>(modes);
+        modeSelector.setFont(new Font("머니그라피TTF Rounded", Font.PLAIN, 10));
 
-        //JTable - 입고 기록 조회
+        inputField.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (inputField.getForeground().equals(Color.GRAY)) {
+                    inputField.setText("");
+                    inputField.setForeground(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (inputField.getText().isEmpty()) {
+                    String selectedMode = (String) modeSelector.getSelectedItem();
+                    if ("전체보기".equals(selectedMode)) {
+                        inputField.setText("전체 내역 조회");
+                    } else if ("자재코드".equals(selectedMode)) {
+                        inputField.setText("자재 코드");
+                    } else if ("제조업체".equals(selectedMode)) {
+                        inputField.setText("제조업체 코드");
+                    } else if ("입고신청일".equals(selectedMode)) {
+                        inputField.setText("YYYY-MM-DD");
+                    }
+                    inputField.setForeground(Color.GRAY);
+                }
+            }
+        });
+
+        modeSelector.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectMode = (String) modeSelector.getSelectedItem();
+                if ("전체보기".equals(selectMode)) {
+                    inputField.setText("전체 내역 조회");
+                    inputField.setForeground(Color.GRAY);
+                    inputField.setFont(new Font("머니그라피TTF Rounded", Font.PLAIN, 10));
+                } else if ("자재코드".equals(selectMode)) {
+                    inputField.setText("자재 코드");
+                    inputField.setForeground(Color.GRAY);
+                    inputField.setFont(new Font("머니그라피TTF Rounded", Font.PLAIN, 10));
+                } else if ("제조업체".equals(selectMode)) {
+                    inputField.setText("제조업체 코드");
+                    inputField.setForeground(Color.GRAY);
+                    inputField.setFont(new Font("머니그라피TTF Rounded", Font.PLAIN, 10));
+                } else if ("입고신청일".equals(selectMode)) {
+                    inputField.setText("YYYY-MM-DD");
+                    inputField.setForeground(Color.GRAY);
+                    inputField.setFont(new Font("머니그라피TTF Rounded", Font.PLAIN, 10));
+                }
+            }
+        });
 
 
-        // JPanel- 입고 신청
-        DefaultTableModel dtm = new DefaultTableModel();
+        //'검색' 버튼 기능 구현
+        JButton searchButton = new JButton("검색");
+        searchButton.setFont((new Font("머니그라피TTF Rounded", Font.PLAIN, 10)));
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                selectMode = (String) modeSelector.getSelectedItem();
+                InputDAO inputDAO = new InputDAO();
+                if (selectMode.equals("전체보기")) {
+                    // 전체 검색 결과
+                } else if (selectMode.equals("자재코드")) {
+                    // 자재코드에 따른 검색 결과
+                    String productCodeInput = inputField.getText();
+                    result();
+                } else if (selectMode.equals("제조업체")) {
+                    // 제조업체에 따른 검색 결과
+                    String manufacturerInput = inputField.getText();
+                    result();
+                } else if (selectMode.equals("입고신청일")) {
+                    // 입고신청일에 따른 검색 결과
+                    String askingDateInput = inputField.getText();
+                    result();
+                }
+            }
+        });
+
+        //생성된 검색 기능 추가
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        topPanel.add(modeSelector);
+        topPanel.add(inputField);
+        topPanel.add(searchButton);
+        topPanel.setBackground(Color.decode("#97A6A0"));
+        topPanel.setBounds(0, 0, 1100, 30);
+        add(topPanel);
+
+        //테이블 추가
+        JPanel middlePanel = new JPanel();
+        middlePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        middlePanel.add(resultTable);
+        middlePanel.setBackground(Color.WHITE);
+        middlePanel.setSize(1100,290);
+        middlePanel.setBounds(0, 30, 1100, 280);
+        add(middlePanel);
 
 
+        //입고 신청
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        bottomPanel.setSize(1100, 145);
+        bottomPanel.setBackground(Color.BLUE);
+        bottomPanel.setBounds(0, 310, 1100, 140);
+        add(bottomPanel);
 
-        JTable inputTable = new JTable(dtm);
-        inputTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-        inputTable.setAutoCreateRowSorter(true);
-        add(inputTable, BorderLayout.CENTER);
 
-        JScrollPane scrollPane = new JScrollPane(inputTable);
-        add(scrollPane);
+        setVisible(true);
+    }
 
 
+    private void result() {
+        InputDAO inputDAO = new InputDAO();
+
+        if (selectMode.equals("전체보기")) {
+            // 전체 검색 결과
+            ArrayList<InputVO> resultAll = inputDAO.getAll();
+
+            String[] columnNames = {"입고번호", "제조업체 코드", "자재 코드", "입고신청일", "입고 수량", "입고일"};
+            DefaultTableModel modelforResult = new DefaultTableModel();
+            for (InputVO vo : resultAll) {
+                Object[] rowForResult = {vo.getInputNum(), vo.getManufacturerCode(), vo.getProductCode(),
+                        vo.getAskingDate(), vo.getWarehousedQuantity(), vo.getWarehousedDate()};
+                modelforResult.addRow(rowForResult);
+            }
+
+            resultTable.setModel(modelforResult);
+        } else if (selectMode.equals("자재코드")) {
+            // 자재코드에 따른 검색 결과
+            String productCodeInput = inputField.getText();
+
+            ArrayList<InputProductVO> resultProductCode = inputDAO.listForProductCode(Integer.parseInt(productCodeInput));
+
+            String[] columnNames = {"자재코드", "자재명", "입고번호", "제조업체 코드", "입고신청일", "입고 수량", "입고일"};
+            DefaultTableModel modelforResult = new DefaultTableModel();
+            for (InputProductVO vo : resultProductCode) {
+                Object[] rowForResult = {vo.getProduct_code(), vo.getProduct_name(), vo.getInput_num(),
+                        vo.getManufacturer_code(), vo.getAsking_date(), vo.getWarehoused_quantity(),
+                        vo.getWarehoused_date()};
+                modelforResult.addRow(rowForResult);
+            }
+
+            resultTable.setModel(modelforResult);
+        } else if (selectMode.equals("제조업체")) {
+            // 제조업체에 따른 검색 결과
+            String manufacturerInput = inputField.getText();
+
+            ArrayList<InputManuVO> resultManu = inputDAO.listForManu(manufacturerInput);
+
+            String[] columnNames = {"제조업체 코드", "제조업체명", "업종", "입고번호", "자재코드", "입고신청일", "입고 수량", "입고일"};
+            DefaultTableModel modelforResult = new DefaultTableModel();
+            for (InputManuVO vo : resultManu) {
+                Object[] rowForResult = {vo.getManufacturer_code(), vo.getManufacturer_name(), vo.getSorting(),
+                        vo.getInput_num(), vo.getProduct_code(), vo.getAsking_date(),
+                        vo.getWarehoused_quantity(), vo.getWarehoused_date()};
+                modelforResult.addRow(rowForResult);
+            }
+
+            resultTable.setModel(modelforResult);
+        } else if (selectMode.equals("입고신청일")) {
+            // 입고신청일에 따른 검색 결과
+            String askingDateInput = inputField.getText();
+
+            ArrayList<InputVO> resultManu = inputDAO.list(askingDateInput);
+
+            String[] columnNames = {"입고신청일", "입고번호", "제조업체 코드", "자재코드", "입고 수량", "입고일"};
+            DefaultTableModel modelforResult = new DefaultTableModel();
+            for (InputVO vo : resultManu) {
+                Object[] rowForResult = {vo.getAskingDate(), vo.getInputNum(), vo.getManufacturerCode(),
+                        vo.getProductCode(), vo.getWarehousedQuantity(), vo.getWarehousedDate()};
+                modelforResult.addRow(rowForResult);
+            }
+
+            resultTable.setModel(modelforResult);
+        }
     }
 }
