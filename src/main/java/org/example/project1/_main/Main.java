ac54149d9b6919e64991285e4f9e2a11374fbca5
양혜선 <hspob819b@gmail.com>
2024-12-02 17:27:59 +0900

    메인 페이지에서 구현 확인

--- src/main/java/org/example/project1/_main/Main.java
+++ src/main/java/org/example/project1/_main/Main.java
@@ -1,12 +1,14 @@
 package org.example.project1._main;
 
 
+import org.example.project1.mainmenu.UI.AdminMenuFrame;
 import org.example.project1.mainmenu.UI.UserMenuFrame;
 import org.example.project1.user.UI.LoginFrame;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         //new LoginFrame();
-        new UserMenuFrame("giho");
+        //new UserMenuFrame("giho");
+        new AdminMenuFrame("hyeseon");
     }
 }
