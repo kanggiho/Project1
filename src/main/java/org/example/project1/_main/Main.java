19c425822e3a43b280d5ab543b231f2f40bd7833
kanggiho <kgh5554@naver.com>
2024-12-03 15:58:16 +0900

    2024 12 03 15 58

--- src/main/java/org/example/project1/_main/Main.java
+++ src/main/java/org/example/project1/_main/Main.java
@@ -2,12 +2,12 @@ package org.example.project1._main;
 import org.example.project1._common.Model.Service.getComboBox;
 import org.example.project1.mainmenu.UI.AdminMenuFrame;
 import org.example.project1.mainmenu.UI.UserMenuFrame;
+import org.example.project1.user.UI.LoginFrame;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        //new LoginFrame();
+        new LoginFrame();
         //new UserMenuFrame("giho");
         //new AdminMenuFrame("hyeseon");
-        new getComboBox();
     }
 }
