c3e0f00a678340e4f22a69724a6cb254ec0362ab
kanggiho <kgh5554@naver.com>
2024-12-04 16:57:04 +0900

    2024-12-04 1657

--- src/main/java/org/example/Project1/dashboard/UI/DashboardMainPanel.java
+++ src/main/java/org/example/Project1/dashboard/UI/DashboardMainPanel.java
@@ -7,26 +7,50 @@ public class DashboardMainPanel extends JPanel {
 
     private RandomProductRecommendationPanel recommendationPanel;
 
+    private PieChartPanel pieChartPanel;
+
+    private JLabel chartTitleLabel;
+
+    private Font font;
 
     public DashboardMainPanel() {
         setLayout(null);  // 절대 위치 지정을 위해 null 레이아웃 사용
         setBackground(Color.WHITE);
 
+        init();
         initComponents();
+        initChart();
         layoutComponents();
         setVisible(true);
+    }
+    private void init() {
+        font = new Font("머니그라피TTF Rounded", Font.PLAIN, 20);
     }
 
     private void initComponents() {
         recommendationPanel = new RandomProductRecommendationPanel();
+
+
+    }
+
+    public void initChart(){
+        pieChartPanel = new PieChartPanel();
+        pieChartPanel.setBounds(570,50,500,500);
+        chartTitleLabel = new JLabel();
+        chartTitleLabel.setText("나의 발주 보기");
+        chartTitleLabel.setFont(font);
+        chartTitleLabel.setBounds(610,30,200,30);
+        add(chartTitleLabel);
+        add(pieChartPanel);
     }
 
     private void layoutComponents() {
         // RandomProductRecommendationPanel 배치
         recommendationPanel.setBounds(0, 0, 550, 150);  // x, y, width, height 조절
         add(recommendationPanel);
+    }
 
-
-
+    public void updateChart(){
+        pieChartPanel.getNew();
     }
 }
